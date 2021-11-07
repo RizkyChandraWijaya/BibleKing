74e77acc28a96b36e4cc60f0aa52a4ab7d6737a8
RizkyChandraWijaya <89894989+RizkyChandraWijaya@users.noreply.github.com>
2021-11-07 14:06:54 +0700

    Update alert and logo

--- tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/Obtineo/App.java
+++ tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/Obtineo/App.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 
 public class App extends Application
 {
@@ -15,6 +15,7 @@ public class App extends Application
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Bible King");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
         stage.setScene(scene);
         stage.show();
     }
