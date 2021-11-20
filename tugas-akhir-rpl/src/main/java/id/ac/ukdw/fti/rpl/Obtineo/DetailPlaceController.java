f7aa1c32bdc6df45575d209718b55e86a8958a32
DeaTrikartikaIndra <89964080+DeaTrikartikaIndra@users.noreply.github.com>
2021-11-21 01:14:07 +0700

    DetailPlaces ver1

--- tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/Obtineo/DetailPlaceController.java
+++ tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/Obtineo/DetailPlaceController.java
@@ -3,9 +3,15 @@ package id.ac.ukdw.fti.rpl.Obtineo;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import id.ac.ukdw.fti.rpl.Obtineo.database.Database;
+import id.ac.ukdw.fti.rpl.Obtineo.modal.Events;
 import id.ac.ukdw.fti.rpl.Obtineo.modal.Places;
 import id.ac.ukdw.fti.rpl.Obtineo.modal.Verses;
 import javafx.collections.FXCollections;
@@ -79,11 +85,16 @@ public class DetailPlaceController implements Initializable{
 
     private ObservableList<Verses> verses = FXCollections.observableArrayList();
     private ObservableList<Places> places = FXCollections.observableArrayList();
+    private ObservableList<Events> events = FXCollections.observableArrayList();
     private String selectedItem = new String(); 
     private List<String> selectedItemVerses = new ArrayList<String>();
     private String displayTitle;
     private String featureType;
     private String eventVerses = "";
+    private String duration;
+    int newDuration;
+    private ArrayList<Integer> arrDuration = new ArrayList<Integer>();
+    Map<String, Integer> durationMap = new HashMap<String, Integer>();
 
 
     //yang dijalankan pertama kali
@@ -128,8 +139,29 @@ public class DetailPlaceController implements Initializable{
             if(places2.getVerses()!=null){
                 for (Verses verses2 : verses) {
                     if(verses2.getTimeline()!=null){
+                        String replaceEventTitle = "LOWER(REPLACE(title,\"'\",\" \"))";
+                        String queryEvent = "SELECT title,verseSort,verses,startDate,duration,predecessor,partOf,places FROM events where "+replaceEventTitle+" like '"+verses2.getTimeline().toLowerCase().replace("'", " ")+"'";
+                        events = Database.instance.getAllEvents(queryEvent);
+                        for(Events events2 : events ){
+                            duration = events2.getDuration();
+                            if(duration != null){
+                                String duration2 = duration.substring(duration.length()-1);                            
+                                if(duration2.equals("D")){
+                                    newDuration = Integer.parseInt(duration.substring(0,duration.length()-1))*1;
+                                }else if(duration2.equals("M")){
+                                    newDuration = Integer.parseInt(duration.substring(0,duration.length()-1))*30;
+                                }else if(duration2.equals("Y")){
+                                    newDuration = Integer.parseInt(duration.substring(0,duration.length()-1))*365;
+                                }
+                            arrDuration.add(newDuration); 
+                                
+                               
+                            }
+                            
+                        }
                         if(verses.indexOf(verses2)==verses.size()-1){
                             eventVerses = eventVerses + verses2.getTimeline();
+                            
                         }else{
                             eventVerses = eventVerses + verses2.getTimeline()+", ";
                         }
@@ -156,7 +188,63 @@ public class DetailPlaceController implements Initializable{
             );
 
         //==============================text=================================
+
+        //==============Pie Chart=====================
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        String[] arrEvent = eventVerses.split(", ");
+        durationMap.put(eventVerses, newDuration);
+
+        for (String ft : arrEvent) { 
+            if (counts.containsKey(ft)) { 
+                counts.put(ft, counts.get(ft) + 1); 
+            } else { 
+                counts.put(ft, 1); 
+            } 
+        }
+        ArrayList<String> arrEvent1 = new ArrayList<String>();
+        ArrayList<Integer> arrDuration1 = new ArrayList<Integer>(); 
+
+        for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+            arrEvent1.add(entry.getKey());
+            arrDuration1.add(entry.getValue());
+        }
+        ObservableList<PieChart.Data> list = FXCollections.observableArrayList();
+        for(int i=0;i<arrEvent1.size(); i++){
+            list.add(new PieChart.Data(arrEvent1.get(i),arrDuration1.get(i)));
+        }
+        placePieChart.setData(list);
+    // System.out.println(list);
     }
+
+    
+    
+
+//     for (String ft : arrFeatureType) { 
+//         if (counts.containsKey(ft)) { 
+//             counts.put(ft, counts.get(ft) + 1); 
+//         } else { 
+//             counts.put(ft, 1); 
+//         } 
+//     } 
+    
+//     ArrayList<String> arrNameFt = new ArrayList<String>();
+//     ArrayList<Integer> arrCountFt = new ArrayList<Integer>();
+    
+//     for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+//         // System.out.println(entry.getKey()+" "+entry.getValue());
+//         arrNameFt.add(entry.getKey());
+//         arrCountFt.add(entry.getValue());
+//     }
+    
+//     ObservableList<PieChart.Data> list = FXCollections.observableArrayList();
+//     for(int i=0;i<arrNameFt.size(); i++){
+//         list.add(new PieChart.Data(arrNameFt.get(i),arrCountFt.get(i)));
+//         }
+//     // System.out.println(list);
+    
+//     eventPieChart.setData(list);
+
+// }
 
     //pindah ke halaman searchingPlaces
     @FXML
